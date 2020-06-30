45de08f580412a52bed41fe58d41cfe4442b7f30
anshuman4luv <anshuman4luv@gmail.com>
2020-06-30 20:33:22 +0530

    Update Config.js

--- config/Config.js
+++ config/Config.js
@@ -8,7 +8,7 @@ module.exports.ODA_WEBHOOK_SECRET = process.env.BOT_WEBHOOK_SECRET || 'XKla5JZQ5
 exports.SMOOCH_APP_ID = process.env.SMOOCH_APP_ID || '5cb03a22ac003b0010331337';
 exports.SMOOCH_KEY_ID = process.env.SMOOCH_KEY_ID || 'app_5cb03d1080314900108d8ee6';
 exports.SMOOCH_SECRET = process.env.SMOOCH_SECRET || 'jftbq3zRfRjR7fsEdpmIMZxOWUC0pbAJp2uGwNE4ZUL5OdkzlPX20aGKVm0LBtnVzxP277mfHZcGRDKAprprcg';
-exports.SMOOCH_WEBHOOK_SECRET = process.env.SMOOCH_WEBHOOK_SECRET || 'it-IISMgDlA7fNNLeoxsPWUGUVGmgSNNE2sqPlAZpY6_Yja1lUyIuQCGEqKtMquYCfCnzOGsnJERKas_ezFHtw';
+exports.SMOOCH_WEBHOOK_SECRET = process.env.SMOOCH_WEBHOOK_SECRET || 'rzPRouKivpRJ5edddO1h0V2r6C9SaRk_KAfHvy1gz1XALXA2SGQNgZt3lNy4Hdtk0oO7RREFDMQrOAczq_UNbQ';
 
 // General Details
 exports.PROXY = process.env.PROXY || process.env.http_proxy;
